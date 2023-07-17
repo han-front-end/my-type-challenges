cc11e17f59cfa43ef1581b90ba64c4ec03a343fc
midsummer <h0109l@outlook.com>
2023-07-17 14:43:02 +0800

    🗑️ refactor: delete build *.js

--- type-challenges/00007-easy-readonly/template.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-// 遍历keyof T的所有索引类型  加上readonly关键字
--- type-challenges/00011-easy-tuple-to-object/test-cases.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'];
-const tupleNumber = [1, 2, 3, 4];
-const tupleMix = [1, '2', 3, '4'];
